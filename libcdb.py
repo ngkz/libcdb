f20614f05a76cf981186d04b80a845da1c08a382
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2014-08-31 20:46:10 +0900

    Add libcdb.search

--- libcdb.py
+++ libcdb.py
@@ -1,5 +1,19 @@
 import sqlite3
 import os.path
+import subprocess
+import re
+
+def get_exports(path):
+    with open(os.devnull, "wb") as null:
+        output = subprocess.check_output(["nm", "-D", path], stderr = null)
+
+    exports = {}
+    for match in re.findall(r"^([0-9a-f]+) *. *(\S*)$", output,
+                            re.MULTILINE | re.IGNORECASE):
+        offset, name = match
+        exports[name] = int(offset, 16)
+
+    return exports
 
 class LibCDB:
     def __init__(self, path = None):
@@ -9,7 +23,6 @@ class LibCDB:
         c = self.conn.cursor()
         try:
             c.execute("""create table libraries(
-                id integer primary key autoincrement,
                 package text unique not null,
                 path text unique not null
             )""")
@@ -34,9 +47,29 @@ class LibCDB:
     def add_library(self, package, path):
         c = self.conn.cursor()
         c.execute("insert into libraries (package, path) values (?, ?)", (package, path))
+        library_id = c.lastrowid
+
+        exports = get_exports(path)
+        for name, offset in exports.items():
+            c.execute("insert into symbols values(?, ?, ?)", (library_id, name, offset))
+
         self.conn.commit()
         c.close()
 
     def search(self, imports):
-        pass
+        c = self.conn.cursor()
+
+        conditions = []
+        _vars = []
+        for name, address in imports.items():
+            _vars.append(name)
+            _vars.append(address & 0xfff)
+            conditions.append("(symbols.name = ? and symbols.offset & 4095 = ?)")
+        _vars.append(len(imports.keys()))
+
+        query = "select libraries.path from libraries, symbols where libraries.rowid = symbols.library_id and (" + " or ".join(conditions) + ") group by symbols.library_id having count(symbols.library_id) = ?"
+        c.execute(query, _vars)
+        result = [row[0] for row in c.fetchall()]
+        c.close()
+        return result
 
--- test_libcdb.py
+++ test_libcdb.py
@@ -3,7 +3,7 @@
 import unittest
 import libcdb
 
-class HasPackageTest(unittest.TestCase):
+class LibcDBTest(unittest.TestCase):
     def setUp(self):
         self.libcdb = libcdb.LibCDB(":memory:")
         self.libcdb.add_library("libc-test1", "testlibs/testlib1.so")
@@ -15,6 +15,25 @@ class HasPackageTest(unittest.TestCase):
         self.assertEquals(self.libcdb.has_package("libc-test2"), True)
         self.assertEquals(self.libcdb.has_package("libc-test3"), True)
         self.assertEquals(self.libcdb.has_package("libc-test4"), False)
+
+    def test_search(self):
+        self.assertEquals(self.libcdb.search({
+            "__libc_start_main": 0x55555320
+        }), ["testlibs/testlib1.so", "testlibs/testlib2.so"])
+        self.assertEquals(self.libcdb.search({
+            "a": 0x55555320
+        }), [])
+        self.assertEquals(self.libcdb.search({
+            "__libc_start_main": 0x55555273
+        }), ["testlibs/testlib3.so"])
+        self.assertEquals(self.libcdb.search({
+            "__libc_start_main": 0x55555320,
+            "write": 0x55555321
+        }), ["testlibs/testlib1.so"])
+        self.assertEquals(self.libcdb.search({
+            "__libc_start_main": 0x55555320,
+            "write": 0x55555322
+        }), ["testlibs/testlib2.so"])
 
 if __name__ == '__main__':
     unittest.main()
